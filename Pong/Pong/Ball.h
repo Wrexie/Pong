964cfa5f6722781b2a089b7e2c4c799696c29b3d
Wrexie <61041168+Wrexie@users.noreply.github.com>
2020-04-18 21:38:11 -0700

    Ball class constructor created

--- Pong/Pong/Ball.h
+++ Pong/Pong/Ball.h
@@ -1,5 +1,15 @@
-#pragma once
-class Ball
+#include <SFML/Graphics.hpp>
+
+class Ball : public sf::CircleShape
 {
+public:
+	Ball(float xCoord, float yCoord)
+		: sf::CircleShape(5), x{ xCoord }, y{ yCoord }{
+		setFillColor(sf::Color::White);
+		setPosition(x, y);
+	}
+
+private:
+	float x, y;
 };
 

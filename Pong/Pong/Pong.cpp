4da66f573661a5d63b584141f7928e31846cc154
Wrexie <61041168+Wrexie@users.noreply.github.com>
2020-04-18 20:46:29 -0700

    VideoMode Object Created, Window rendered, main game loop created

--- Pong/Pong/Pong.cpp
+++ Pong/Pong/Pong.cpp
@@ -7,8 +7,17 @@ using namespace sf;
 
 int main()
 {
+	//Video Mode Object
 	VideoMode vm(800, 600);
 
+	//Render the window
 	RenderWindow window(vm, "Pong - By: Wrexie", Style::Default);
+
+	//Main game loop
+	while (window.isOpen()) {
+		if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+			window.close();
+		}
+	}
 }
 

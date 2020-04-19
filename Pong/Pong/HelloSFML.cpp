c3989dc16b9be5085bd0fb69870a2dfce8221325
Wrexie <61041168+Wrexie@users.noreply.github.com>
2020-04-18 20:39:10 -0700

    Initial Project File Commit

---
+++ Pong/Pong/HelloSFML.cpp
@@ -0,0 +1,14 @@
+
+#include "pch.h"
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+int main()
+{
+	VideoMode vm(800, 600);
+
+	RenderWindow window(vm, "Pong - By: Wrexie", Style::Default);
+}
+
